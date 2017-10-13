18f5ec0f8eb173494d6f1d63fc476f9924b0bf9d
hazzyof <kondratiuk011@gmail.com>
2017-10-13 18:45:42 +0300

    Second commit
    What's new?
    -added main menu
    -added character and its movement controller

---
+++ Fish_Root/Assets/Scripts/characterMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+    public GameObject player;
+    public int speedRotation = 3;
+    public int speed = 5;
+
+    void Start()
+    {
+        player = (GameObject)this.gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += player.transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.position -= player.transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Rotate(Vector3.down * speedRotation);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Rotate(Vector3.up * speedRotation);
+        }
+    }
+}
---
+++ Fish_Root/Assets/Scripts/menuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuScript : MonoBehaviour {
+
+    public GameObject menuPanel;
+
+    void Start ()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void onClickStart()
+    {
+        menuPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void onClickExit()
+    {
+        Application.Quit();
+    }
+}
